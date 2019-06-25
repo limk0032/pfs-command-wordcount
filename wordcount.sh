12c3a18459b76fd4f59b75e82649b83813b25827
limk0032 <limk0032@gmail.com>
2019-06-25 14:29:09 +0800

    Update wordcount.sh

--- wordcount.sh
+++ wordcount.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tr ' ' '\n' | sort | uniq -c | sort -n
-# tr ' ' '\n' | sort | grep -Ev '^(a|of|in|is|to)$' | uniq -c | sort -n
+# tr ' ' '\n' | sort | uniq -c | sort -n
+tr ' ' '\n' | sort | grep -Ev '^(a|of|in|is|to)$' | uniq -c | sort -n
